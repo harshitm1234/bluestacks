65f5af190846fbf093a8e0baaf5fd243c6e9456b
Harshit Mehndiratta <harshitm1234>
2020-11-14 23:59:58 +0530

    Unused Remove

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import "./App.css";
 import FixedHeader from "./Components/Fixed Header/fixedheader";
 import Header from "./Components/Header/Header";
